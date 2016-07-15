eef4229eee8a9115f29181ca3a513d5f45c0e8fa
pdurangop <pdurangop@gmail.com>
2016-07-14 20:53:54 -0500

    setting the first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    resources :vehicles
+  end
+
+  resources :locations do
+    resources :slots
+  end
+
 end
