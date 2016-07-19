fc51534eb3b206c60051ca481d786526fbcebdee
pdurangop <pdurangop@gmail.com>
2016-07-18 20:56:50 -0500

    render the views User on loggin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  respond_to :html, :json
+
   protect_from_forgery with: :exception
+
+  before_filter :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :email, :password, :birthdate])
+  end
+  protected :configure_permitted_parameters
 end
